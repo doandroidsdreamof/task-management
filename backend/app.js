58e7695f595f3439b4a0bb6505a7779c50e05c6f
doandroidsdreamof <mutlukuytuoglu@gmail.com>
2023-02-16 20:20:25 +0300

    passoword hashing test

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const connectDB = require('./config/db');
 const registerRouter = require('./routes/autRouter');
-const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors')
 const bcrypt = require('bcryptjs');
@@ -23,9 +22,8 @@ app.use(cors(corsOptions));
 
 //middleware
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(registerRouter);
+
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
