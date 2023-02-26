5a942cf7b6be5f56d6fbd0b01e6823507b446233
doandroidsdreamof <mutlukuytuoglu@gmail.com>
2023-02-26 23:25:34 +0300

    notification ref && close logic debugging

--- frontend/src/components/common/Notification.tsx
+++ frontend/src/components/common/Notification.tsx
@@ -18,7 +18,6 @@ const Notification = () => {
     <>
       <button
         onClick={() => setToggle(!toggle)}
-        ref={notificationRef}
         type='button'
         data-dropdown-toggle='notification-dropdown'
         className='p-2 mr-1 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600'
